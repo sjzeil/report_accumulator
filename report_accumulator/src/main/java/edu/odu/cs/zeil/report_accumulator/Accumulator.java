831c28295a17ce2247f052b0d8ca3613a38b354b
Steven Zeil <zeil@cs.odu.edu>
2016-07-30 11:44:59 -0400

    Incorporated changes from main branch

--- report_accumulator/src/main/java/edu/odu/cs/zeil/report_accumulator/Accumulator.java
+++ report_accumulator/src/main/java/edu/odu/cs/zeil/report_accumulator/Accumulator.java
@@ -17,145 +17,190 @@ import java.util.ArrayList;
 /**
  * Accumulate development statistics for trend plotting.
  *  
- * Scans a set of report directories to locate common testing and analysis reports, 
- * accumulating statistics where appropriate and adding those point statistics for
- * the current build to to csv files maintained on a project web site.
+ * Scans a set of report directories to locate common testing and analysis
+ * reports, accumulating statistics where appropriate and adding those point
+ * statistics for the current build to to csv files maintained on a project
+ * web site.
  * 
  * @author zeil
  *
  */
 public class Accumulator {
-	
-	private URL projectWebsite;
-	private Path[] reportDirectories;
-	private ArrayList<ReportScanner> scanners;
-	
-	/**
-	 * Create an accumulator for a given web site and local report directory.
-	 *  
-	 * @param projectWebsite URL for the project web site corresponding to the report directory
-	 * @param reportDirectories a set of directories that may contain newly generated reports. Each
-	 *     report must reside in a sub-directory within one of these directories.
-	 */
-	public Accumulator (
-			URL projectWebsite,
-			Path[] reportDirectories) {
-		this.projectWebsite = projectWebsite;
-		this.reportDirectories = reportDirectories;
-		scanners = new ArrayList<>();
-	}
 
-	
-	
-	/**
-	 * Add a known type of report to the scanning process.
-	 * 
-	 * @param reportKind a scanner for a specific type of report.
-	 */
-	public void register (ReportScanner reportKind) {
-		scanners.add(reportKind);
-	}
-	
-	
-	/**
-	 * Scan the report directories for sub-directories containing common reports
-	 * If reportDir/subDir holds a report for which statistics are appropriate,
-	 * try to obtain a file projectWebSiteURL/reportDir/subDir.csv containing prior
-	 * stats. If none exists, create a new such file. Add current statistics to the
-	 * end of this CSV structure and save in reportDir/subDir.csv
-	 * 
-	 * The current reports and updated statistics can then be uploaded to the web site,
-	 * though doing so is outside the scope of this class.  It is assumed that such
-	 * uploading is handled as part of the normal project build. 
-	 */
-	public void accumulateStatistics () {
-		for (Path reportDir: reportDirectories) {
-			for (File dir: reportDir.toFile().listFiles()) {
-				if (dir.isDirectory()) {
-					for (ReportScanner scanner: scanners) {
-						scanForStatistics (dir, scanner);
-					}
-				}
-			}
-		}
-	}
+    /**
+     * An ID string for the current build.
+     */
+    private String buildID;
 
+    /**
+     * URL of the project website, possibly already containing CSV files
+     * holding statistics from prior builds.
+     */
+    private URL projectWebsite;
 
-	/**
-	 * Attempt to apply a single report scanner to a directory.
-	 * @param dir  possible report directory
-	 * @param scanner a scanner for a specific type of report
-	 */
-	private void scanForStatistics(File dir, ReportScanner scanner) {
-		scanner.setDirectory(dir.toPath());
-		if (scanner.containsReport()) {
-			double[] pointStatistics = scanner.extractStatistics();
-			if (pointStatistics != null && pointStatistics.length > 0) {
-				String reportName = dir.getName() + ".csv";
-				String existingContent;
-				int lineCount = 0;
-				try {
-					URL existingReportURL = new URL(projectWebsite.toString() + "/" + reportName);
-					try (BufferedReader in = new BufferedReader(new InputStreamReader(existingReportURL.openStream()))) {
-						StringBuffer buf = new StringBuffer();
-						String line;
-						while ((line = in.readLine()) != null) {
-							buf.append(line);
-							buf.append("\n");
-							++lineCount;
-						}
-						existingContent = buf.toString();
-					} catch (IOException e) {
-						existingContent = "";
-					}
-				} catch (MalformedURLException e) {
-					existingContent = "";
-				}
-				
-				try (BufferedWriter out = new BufferedWriter(new FileWriter(new File(dir.getParentFile(), reportName)))) {
-					if (existingContent.length() > 0) {
-						out.write(existingContent);
-					} else {
-						out.write(dir.getName());
-						for (String s: scanner.getDescriptors()) {
-							out.write(",");
-							out.write(s);
-						}
-						out.newLine();
-					}
-					out.write(new Integer(lineCount+1).toString());
-					for (double stat: pointStatistics) {
-						out.write(",");
-						out.write(new Double(stat).toString());
-					}
-					out.newLine();
-				} catch (IOException e) {
-					System.err.println("Problem writing out statistics to " + reportName + ": " + e);
-					e.printStackTrace();
-				}
-			}
+    /**
+     * Directories in which to search for project reports.
+     */
+    private Path[] reportDirs;
 
-		}
-	}
+    /**
+     * Scanners customized for each known type of report.
+     */
+    private ArrayList<ReportScanner> scanners;
+
+    /**
+     * Create an accumulator for a given web site and local report directory.
+     *  
+     * @param aBuildID identifying string for this build.
+     * @param aProjectWebsite URL for the project web site corresponding to the
+     *           report directory
+     * @param reportDirectories a set of directories that may contain newly
+     *           generated reports. Each report must reside in a sub-directory
+     *           within one of these directories.
+     */
+    public Accumulator (
+            final String aBuildID,
+            final URL aProjectWebsite,
+            final Path[] reportDirectories) {
+        this.buildID = aBuildID;
+        this.projectWebsite = aProjectWebsite;
+        this.reportDirs = reportDirectories;
+        scanners = new ArrayList<>();
+    }
 
 
 
-	/**
-	 * 
-	 * @return the project website URL.
-	 */
-	public URL getProjectWebsite() {
-		return projectWebsite;
-	}
+    /**
+     * Add a known type of report to the scanning process.
+     * 
+     * @param reportKind a scanner for a specific type of report.
+     */
+    public final void register (final ReportScanner reportKind) {
+        scanners.add(reportKind);
+    }
 
 
-	/**
-	 * 
-	 * @return the local report directories.
-	 */
-	public Path[] getReportDirectories() {
-		return reportDirectories;
-	}
+    /**
+     * Scan the report directories for sub-directories containing common reports
+     * If reportDir/subDir holds a report for which statistics are appropriate,
+     * try to obtain a file projectWebSiteURL/reportDir/subDir.csv containing
+     * prior statistics. If none exists, create a new such file. Add current
+     * statistics to the end of this CSV structure and save in
+     * reportDir/subDir.csv
+     * 
+     * The current reports and updated statistics can then be uploaded to the
+     * web site, though doing so is outside the scope of this class.  It is
+     * assumed that such uploading is handled as part of the normal project
+     * build. 
+     */
+    public final void accumulateStatistics () {
+        for (Path reportDir: reportDirs) {
+            for (File dir: reportDir.toFile().listFiles()) {
+                if (dir.isDirectory()) {
+                    for (ReportScanner scanner: scanners) {
+                        scanForStatistics (dir, scanner);
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Attempt to apply a single report scanner to a directory.
+     * @param dir  possible report directory
+     * @param scanner a scanner for a specific type of report
+     */
+    private void scanForStatistics(
+            final File dir, 
+            final ReportScanner scanner) {
+        scanner.setDirectory(dir.toPath());
+        if (scanner.containsReport()) {
+            double[] pointStatistics = scanner.extractStatistics();
+            if (pointStatistics != null && pointStatistics.length > 0) {
+                String reportName = dir.getName() + ".csv";
+                String existingContent;
+                int lineCount = 0;
+                try {
+                    URL existingReportURL = new URL(projectWebsite.toString() 
+                            + "/" + reportName);
+                    try (BufferedReader in = 
+                            new BufferedReader(
+                                    new InputStreamReader(
+                                            existingReportURL.openStream()))) {
+                        StringBuffer buf = new StringBuffer();
+                        String line;
+                        while ((line = in.readLine()) != null) {
+                            buf.append(line);
+                            buf.append("\n");
+                            ++lineCount;
+                        }
+                        existingContent = buf.toString();
+                    } catch (IOException e) {
+                        existingContent = "";
+                    }
+                } catch (MalformedURLException e) {
+                    existingContent = "";
+                }
+
+                if (buildID == null || buildID.trim().length() == 0) {
+                    buildID = Integer.toString(lineCount + 1);
+                } else {
+                    final int maxBuildLabelWidth = 8;
+                    if (buildID.length() > maxBuildLabelWidth) {
+                        buildID = buildID.substring(0,  maxBuildLabelWidth);
+                    }
+                }
+
+                try (BufferedWriter out = 
+                        new BufferedWriter(
+                                new FileWriter(
+                                        new File(dir.getParentFile(), 
+                                                reportName)))) {
+                    if (existingContent.length() > 0) {
+                        out.write(existingContent);
+                    } else {
+                        out.write(dir.getName());
+                        for (String s: scanner.getDescriptors()) {
+                            out.write(",");
+                            out.write(s);
+                        }
+                        out.newLine();
+                    }
+                    out.write(buildID);
+                    for (double stat: pointStatistics) {
+                        out.write(",");
+                        out.write(new Double(stat).toString());
+                    }
+                    out.newLine();
+                } catch (IOException e) {
+                    System.err.println("Problem writing out statistics to " 
+                                       + reportName + ": " + e);
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+
+
+    /**
+     * 
+     * @return the project website URL.
+     */
+    public URL getProjectWebsite() {
+        return projectWebsite;
+    }
+
+
+    /**
+     * 
+     * @return the local report directories.
+     */
+    public Path[] getReportDirectories() {
+        return reportDirs;
+    }
 
 
 }
--- report_accumulator/src/main/java/edu/odu/cs/zeil/report_accumulator/CheckstyleScanner.java
+++ report_accumulator/src/main/java/edu/odu/cs/zeil/report_accumulator/CheckstyleScanner.java
@@ -11,88 +11,105 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 /**
- * Report scanner for Checkstyle reports. Accumulates data on number of warnings issud.
+ * Report scanner for Checkstyle reports. Accumulates data on number
+ * of warnings issued.
  * 
  * @author zeil
  *
  */
 public class CheckstyleScanner implements ReportScanner {
-	
-	Path reportDirectory;
-	Document doc;
 
-	/* (non-Javadoc)
-	 * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#setDirectory(java.nio.file.Path)
-	 */
-	@Override
-	public void setDirectory(Path possibleReportDirectory) {
-		reportDirectory = possibleReportDirectory;
-		doc = null;
-	}
+    /**
+     * Directory containing the (possible) report.
+     */
+    private Path reportDirectory;
+    
+    /**
+     * XML DOM document for entire report.
+     */
+    private Document doc;
 
-	/* (non-Javadoc)
-	 * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#containsReport()
-	 */
-	@Override
-	public boolean containsReport() {
-		double [] results = extractStatistics();
-		return results != null && results.length > 0;
-	}
+    /* (non-Javadoc)
+     * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#setDirectory(java.nio.file.Path)
+     */
+    @Override
+    public final void setDirectory(Path possibleReportDirectory) {
+        reportDirectory = possibleReportDirectory;
+        doc = null;
+    }
 
-	private boolean readDOM(File xmlFile) {
-		if (doc == null) {
-			doc = new DOMParser().readDOM(xmlFile);
-			return (doc != null);
-		} else {
-			return true;
-		}
-	}
+    /* (non-Javadoc)
+     * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#containsReport()
+     */
+    @Override
+    public final boolean containsReport() {
+        double[] results = extractStatistics();
+        return results != null && results.length > 0;
+    }
 
-	/* (non-Javadoc)
-	 * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#extractStatistics()
-	 */
-	@Override
-	public double[] extractStatistics() {
-		if (reportDirectory.toFile().exists()) {
-			for (File xmlFile: reportDirectory.toFile().listFiles()) {
-				if (xmlFile.getName().endsWith(".xml")) {
-					double[] result = extractStatistics(xmlFile);
-					if (result != null && result.length > 0) {
-						return result;
-					}
-				}
-			}
-		}
-		return new double[0];
-	}
+    /**
+     * Read the report if we have not already done so.
+     * @param xmlFile report file
+     * @return DOM of the report
+     */
+    private boolean readDOM(final File xmlFile) {
+        if (doc == null) {
+            doc = new DOMParser().readDOM(xmlFile);
+            return (doc != null);
+        } else {
+            return true;
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#extractStatistics()
+     */
+    @Override
+    public double[] extractStatistics() {
+        if (reportDirectory.toFile().exists()) {
+            for (File xmlFile: reportDirectory.toFile().listFiles()) {
+                if (xmlFile.getName().endsWith(".xml")) {
+                    double[] result = extractStatistics(xmlFile);
+                    if (result != null && result.length > 0) {
+                        return result;
+                    }
+                }
+            }
+        }
+        return new double[0];
+    }
 
 
-	
-	private double[] extractStatistics(File xmlFile) {
-		if (xmlFile.exists()) {
-			if (readDOM(xmlFile)) {
-				Element root = doc.getDocumentElement();
-				if (root.getTagName().equals("checkstyle")) {
-					NodeList details = root.getElementsByTagName("error");
-					int count = details.getLength();
-					double[] result = new double[1];
-					result[0] = (double)count;
-					return result;
-				}
-			}
-		}
-		return null;
-	}
+    /**
+     * Attempt to extract the statistics from a possible report file. 
+     * @param xmlFile
+     * @return statistics if found or an empty array if not found.
+     */
+    private double[] extractStatistics(final File xmlFile) {
+        if (xmlFile.exists()) {
+            if (readDOM(xmlFile)) {
+                Element root = doc.getDocumentElement();
+                if (root.getTagName().equals("checkstyle")) {
+                    NodeList details = root.getElementsByTagName("error");
+                    int count = details.getLength();
+                    double[] result = new double[1];
+                    result[0] = (double) count;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
 
-	
-	/* (non-Javadoc)
-	 * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#getDescriptors()
-	 */
-	@Override
-	public String[] getDescriptors() {
-		String[] result = new String[1];
-		result[0] = "Warnings";
-		return result;
-	}
+
+    /* (non-Javadoc)
+     * @see edu.odu.cs.zeil.report_accumulator.ReportScanner#getDescriptors()
+     */
+    @Override
+    public final String[] getDescriptors() {
+        String[] result = new String[1];
+        result[0] = "Warnings";
+        return result;
+    }
 
 }
